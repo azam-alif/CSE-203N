62f43c431cae2c7177d7d90d4dede7ef61369c11
azam-alif <azamalif483@gmail.com>
2025-03-04 19:36:17 +0600

    assignment

---
+++ 2021132040.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int i, n, evens = 0, odds = 0, zeros= 0;
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+    if (n<=0){
+        printf("Invalid input.\n");
+        return 0;
+    }
+    int num[n];
+    printf("Enter the numbers: ");
+    for(i=0; i<n; i++){
+        scanf("%d", &num[i]);
+    }
+    printf("\n\n");
+    for(i=0; i<n; i++){
+        if(num[i] == 0){
+            printf("Zero: %d\n", num[i]);
+            zeros++;
+        } else if(num[i]%2==0){
+            printf("Even: %d, Square: %d\n", num[i], num[i]*num[i]);
+            evens++;
+        } else if(num[i]%2!=0){
+            printf("Odd: %d, Cube: %d\n", num[i], num[i]*num[i]*num[i]);
+            odds++;
+        }
+    }
+    printf("Total Even Numbers: %d\n",evens);
+    printf("Total Odd Numbers: %d\n",odds);
+    printf("Total Zeros: %d\n",zeros);
+    return 0;
+}
